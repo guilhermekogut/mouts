ba0d8fc0749b361854d952adca92b82f3e72f993
Guilherme Kogut <guilherme@infoworker.com.br>
2025-05-31 21:17:47 -0300

    fixed authentication: AuthenticateUserRequest and AuthenticateUserResult Map was missing

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/AuthenticateUserResultProfile.cs
@@ -0,0 +1,14 @@
+﻿using Ambev.DeveloperEvaluation.Application.Auth.AuthenticateUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Auth.AuthenticateUserFeature;
+
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings;
+
+public class AuthenticateUserResultProfile : Profile
+{
+    public AuthenticateUserResultProfile()
+    {
+        CreateMap<AuthenticateUserResult, AuthenticateUserResponse>();
+    }
+}
